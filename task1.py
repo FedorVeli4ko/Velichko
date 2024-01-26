a52b627421b8f1bf3abe985e9dc88acec47cebcd
itclass <fedor.velichko.2006@mail.ru>
2024-01-26 12:12:22 +0300

    вроде работает

---
+++ task1.py
@@ -0,0 +1,15 @@
+"""
+Решение задачи 1 предпрофессионального экзамена.
+Программа читает данные из csv-файла, ищет результат учащегося.
+Затем подсчитывает средний балл, хаменяет им результаты None и сохраняет в новый файл.
+"""
+
+from csv import reader
+
+with open('students.csv', encoding='utf-8') as data_file:
+    csv_data = reader(data_file, delimiter=',')
+    for row in csv_data:
+        if "Хадаров Владимир" in row[1]:
+            print(f'Ты получил: {row[4]}, за проект - {row[2]}')
+
+
